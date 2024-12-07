c7a591bf3c0970f49da54510e29fcd1e47ea50e2
PiangpornAtWork <piangatwork@gmail.com>
2024-12-07 15:41:21 +0700

    "first commit"

---
+++ backend/controller/genders.go
@@ -0,0 +1,32 @@
+package controller
+
+
+import (
+
+   "net/http"
+
+
+   "example.com/test/config"
+
+   "example.com/test/entity"
+
+   "github.com/gin-gonic/gin"
+
+)
+
+
+func GetAll(c *gin.Context) {
+
+
+   db := config.DB()
+
+
+   var genders []entity.Genders
+
+   db.Find(&genders)
+
+
+   c.JSON(http.StatusOK, &genders)
+
+
+}
